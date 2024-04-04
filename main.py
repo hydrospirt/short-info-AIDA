ed206363315503ea8ff0241002485b7fe188171d
Eduard Gumen <hydrospirt@mail.ru>
2024-04-04 23:36:36 +0300

    Add play_sound

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
+
+from kivy.config import Config
+
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
-from kivy.config import Config
-from kivy.uix.label import Label
+from kivy.core.audio import SoundLoader
+from kivy.uix.button import Button
 
 __version__ = '0.1'
 
@@ -13,13 +16,21 @@ class ShortInfo(GridLayout):
         super(ShortInfo, self).__init__(**var_args)
         Window.size = (640, 340)
         Config.set('graphics', 'resizable', False)
+        Config.write()
 
 
 class ShortInfoApp(App):
     def build(self):
-        self.title = f'Short Info AIDA {__version__}'
-        self.icon = 'icon/icon.png'
+        self.title = f'Short Info AIDA v{__version__}'
+        self.icon = 'assets/icon/icon.png'
+        # self.play_music()
         return ShortInfo()
+
+    def play_music(self):
+        sound_loader = SoundLoader.load('assets/sound/At_Dooms_Gate.mp3')
+        sound_loader.loop = True
+        if sound_loader:
+            sound_loader.play()
 
 
 if __name__ == '__main__':
