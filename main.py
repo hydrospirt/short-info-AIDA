b7a8f3599eda4d79addd2e291822a6fc23f6bdb4
Eduard Gumen <hydrospirt@mail.ru>
2024-04-04 21:46:41 +0300

    Update name app and icon

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import os
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.core.window import Window
 from kivy.config import Config
+from kivy.uix.label import Label
 
 __version__ = '0.1'
 
-Config.set('kivy', 'resizable', False)
-
 
 class ShortInfo(GridLayout):
-    file_path = ...
+    def __init__(self, **var_args):
+        super(ShortInfo, self).__init__(**var_args)
+        Window.size = (640, 340)
+        Config.set('graphics', 'resizable', False)
 
 
 class ShortInfoApp(App):
     def build(self):
+        self.title = f'Short Info AIDA {__version__}'
+        self.icon = 'icon/icon.png'
         return ShortInfo()
 
 
