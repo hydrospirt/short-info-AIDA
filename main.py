d3b597daf5109a35cb0278a80c9a3d617acb9015
Eduard Gumen <hydrospirt@mail.ru>
2024-04-07 18:32:36 +0300

    Add parser data in parse_html func

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 
-__version__ = '0.1'
+__version__ = '0.1 alpha'
+
+
+class ReportPopup(Popup):
+    pass
 
 
 class CustomPopup(Popup):
@@ -31,21 +35,36 @@ class ShortInfo(GridLayout):
             return self.send_error_msg(data)
         with open(os.path.relpath(data), 'r') as f:
             html = BeautifulSoup(f.read(), 'html.parser')
-        return html
+        return self.parse_html(html)
 
     def send_error_msg(self, data):
         popup = CustomPopup()
         popup.open()
 
     def parse_html(self, html):
-        ...
+        table = html.find_all('table')
+        trs = []
+        for td in table:
+            trs.append(td.find_all('tr'))
+        self.report_title = html.title.text
+        report_date = trs[3][11].text
+        name_pc = trs[3][8].text
+        name_user_report = trs[3][9].text
+        monitor = trs[3][25].text
+        oc = trs[3][3].text
+        cpu = trs[3][14].text
+        motherboard = trs[3][15].text
+        videoadapter = trs[3][22].text
+        memory = trs[3][17].text
+        report = ReportPopup()
+        report.open()
 
 
 class ShortInfoApp(App):
     def build(self):
         self.title = f'Short Info AIDA v{__version__}'
         self.icon = 'assets/icon/icon.png'
-        self.play_music()
+        # self.play_music()
         return ShortInfo()
 
     def play_music(self):
