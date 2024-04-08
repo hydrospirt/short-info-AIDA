466acd6065f79e78991a4efcb78eb52f52eeaec6
Eduard Gumen <hydrospirt@mail.ru>
2024-04-08 19:44:56 +0300

    Add copy_results

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from kivy.core.window import Window
 from kivy.core.audio import SoundLoader
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.clipboard import Clipboard
 
 
 __version__ = '0.1 alpha'
@@ -24,6 +25,10 @@ class ResultScreen(Screen):
     def get_back_and_clean(self, results):
         self.input_data.text = ''
         self.manager.current = 'ShortInfo'
+
+    def copy_results(self):
+        text = self.input_data.text
+        Clipboard.copy(text)
 
 
 class CustomPopup(Popup):
